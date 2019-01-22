406f6e09f345abfe26e13de1111e2732b9ec8e2a
Manas Patel <manas.patel@outlook.com>
2019-01-22 08:46:57 +0530

    created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { BookCardComponent } from './components/common/book-card/book-card.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BooksHomeComponent } from './components/books-home/books-home.component';
+import { BookCartComponent } from './components/book-cart/book-cart.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookCardComponent,
+    HeaderComponent,
+    BooksHomeComponent,
+    BookCartComponent
   ],
   imports: [
     BrowserModule,
